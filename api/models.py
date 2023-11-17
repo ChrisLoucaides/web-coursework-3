208766848b77c47dcfdb88cf6ef3b1ead9da259f
Niamh F <ec20611@qmul.ac.uk>
2023-11-17 15:25:26 +0000

    WEB-1 Niamh Created User Model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+
+class SiteUser(AbstractUser):
+    profile_picture = models.ImageField()
+    date_of_birth = models.DateField()
+
+    
