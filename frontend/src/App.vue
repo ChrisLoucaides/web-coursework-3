0a416c672d3a61f439f0b949b784173810bec882
ChrisLoucaides <christosloucaides@gmail.com>
2023-12-11 00:45:12 +0000

    WEB-9 Christos Added navbar/basic styling

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,45 +1,44 @@
 <template>
-    <main class="container pt-4">
-        <div>
-            <router-link
-                    class=""
-                    :to="{name: 'Main Page'}"
-            >
-                Main Page
-            </router-link>
-            |
-            <router-link
-                    class=""
-                    :to="{name: 'Other Page'}"
-            >
-                Other Page
-            </router-link>
-        </div>
-        <RouterView class="flex-shrink-0"/>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-success">
+      <div class="container">
+         <router-link class="navbar-brand" :to="{ name: 'Main Page' }"><img src="./assets/Sky%20News%202.png" alt="" style="width: 10em;"></router-link>
+         <router-link class="navbar-brand" :to="{ name: 'Other Page' }" style="color: white;">Other Page</router-link>
 
+        <button @click="logout" class="btn btn-danger ml-auto">Logout</button>
+      </div>
+    </nav>
+
+    <main class="container pt-4">
+      <RouterView class="flex-shrink-0"/>
     </main>
+  </div>
 </template>
 
 <script lang="ts">
-
 import ArticleView from './components/ArticleView.vue';
-import {defineComponent,} from "vue";
-import {RouterView} from "vue-router";
-
+import { defineComponent } from 'vue';
+import { RouterView } from 'vue-router';
+import { useAuthStore } from '../auth.ts';
 
 export default defineComponent({
-    components: {RouterView, ArticleView},
-    name: "app",
+  components: { RouterView, ArticleView },
+  name: 'app',
+  methods: {
+    logout() {
+      const authStore = useAuthStore();
+      authStore.logout();
+      this.$router.push({ name: 'Django Login' });
+    },
+  },
 });
-
 </script>
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Nunito:wght@200;400;700&display=swap');
-@import url("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css");
-
+@import url('https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css');
 
 body {
-    font-family: 'Nunito', sans-serif !important;
+  font-family: 'Nunito', sans-serif !important;
 }
 </style>
